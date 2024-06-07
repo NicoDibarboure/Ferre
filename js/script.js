5d0ca75149096a353ef907559ba52aa3649d3639
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-06-07 20:29:17 -0300

    Creando carrito de compras y finalizada la pagina detallada de articulos.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ function menuToggle() {
     MenuItems.style.maxHeight = "0px";
   }
 }
+
+var productImg = document.getElementById("product-img");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+  productImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  productImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  productImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  productImg.src = smallImg[3].src;
+};
